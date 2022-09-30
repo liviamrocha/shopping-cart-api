666862802ed1f172d214b8d0e876a41afbbca720
liviamrocha <liviarochaa00@gmail.com>
2022-09-30 13:36:17 -0300

    Add API routes and database connection

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from shopping_cart.routers import api_router
+from shopping_cart.core.settings import get_environment_variables
+from shopping_cart.server.database import connect_db, close_conn_db
+
+env = get_environment_variables()
+app = FastAPI(title=env.APP_NAME)
+
+# Add conection database
+app.add_event_handler('startup', connect_db)
+app.add_event_handler('shutdown', close_conn_db)
+
+# Add API routes
+app.include_router(api_router)
