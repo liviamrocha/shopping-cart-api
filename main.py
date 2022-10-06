e406c574e5b2a9a098443883c6428f4f4ee265f6
liviamrocha <liviarochaa00@gmail.com>
2022-10-06 12:01:04 -0300

    refactor: Add API version

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from shopping_cart.core.settings import get_environment_variables
 from shopping_cart.server.database import connect_db, close_conn_db
 
 env = get_environment_variables()
-app = FastAPI(title=env.APP_NAME)
+app = FastAPI(
+    title=env.APP_NAME,
+    version=1.0
+)
 
 # Add conection database
 app.add_event_handler('startup', connect_db)
