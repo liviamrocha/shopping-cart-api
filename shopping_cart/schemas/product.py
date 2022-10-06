2d07622f94cf23c1562e502133dfb290eb392afb
liviamrocha <liviarochaa00@gmail.com>
2022-10-05 21:05:54 -0300

    feat: Add product schema

--- shopping_cart/schemas/product.py
+++ shopping_cart/schemas/product.py
@@ -1,27 +1,16 @@
 from pydantic import BaseModel, Field
 
-
-# Modelo base de um produto (para cadastro)
 class ProductSchema(BaseModel):
-    name: str
+    name: str = Field(max_length=100)
     description: str
     price: float
-    material: str
-    inmetro: str
-    code: str = Field(unique=True)
-    stock: int
+    image: str
+    code: int 
 
-# Modelo de retorno de um documento de produto
-def product_helper(product) -> dict:
-    return {
-        "id": str(product["_id"]),
-        "name": product["name"],
-        "description": product["description"],
-        "price": product["price"],
-        "material": product["material"],
-        "inmetro": product["inmetro"],
-        "code": product["code"],
-        "stock": product["stock"],
-    }
-    
+class ProductUpdateSchema(BaseModel):
+    name: str = Field(max_length=100)
+    description: str
+    price: float
+    image: str
+
 
