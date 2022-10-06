b8f713d2fcc55a46cb793eb1540636e244f7c5bd
liviamrocha <liviarochaa00@gmail.com>
2022-10-05 22:09:39 -0300

    feat: Add new schema to product update

--- shopping_cart/schemas/product.py
+++ shopping_cart/schemas/product.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 class ProductSchema(BaseModel):
     name: str = Field(max_length=100)
@@ -8,9 +9,20 @@ class ProductSchema(BaseModel):
     code: int 
 
 class ProductUpdateSchema(BaseModel):
-    name: str = Field(max_length=100)
-    description: str
-    price: float
-    image: str
+    name: Optional[str]
+    description: Optional[str]
+    price: Optional[float]
+    image: Optional[str]
+
+
+# Modelo de retorno de um documento de product
+def product_helper(product) -> dict:
+    return{
+        "name": product["name"],
+        "description": product["description"],
+        "price": product["price"],
+        "image": product["image"],
+        "code": product["code"],
+    }
 
 
