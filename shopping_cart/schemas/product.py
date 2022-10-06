56473db37b46bc851b875bb79095261cb5d9f779
liviamrocha <liviarochaa00@gmail.com>
2022-10-06 11:50:56 -0300

    refactor: Add extra settings to Swaegger examples

--- shopping_cart/schemas/product.py
+++ shopping_cart/schemas/product.py
@@ -3,16 +3,27 @@ from typing import Optional
 from shopping_cart.schemas.utils import AllOptional
 
 class ProductSchema(BaseModel):
-    name: str = Field(max_length=100)
-    description: str
-    price: float = Field(gt=0.01)
-    image: str
-    code: int 
+    name: str = Field(max_length=100, description="Nome do produto")
+    description: str = Field(description="Descrição do produto")
+    price: float = Field(gt=0.01, description="Preço do produto")
+    image: str = Field(description="Imagem do produto")
+    code: int = Field(description="Código do produto")
+
+    # Configurações extra para o Swagger
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Produto 1",
+                "description": "Descrição do produto 1",
+                "price": 10.99,
+                "image": "produto1.jpeg",
+                "code": 1
+            }
+        }
 
 class ProductUpdateSchema(ProductSchema, metaclass=AllOptional):
     price: float = Field(gt=0.01)
 class ProductResponse(ProductSchema):
     pass
 
-
-
+    
