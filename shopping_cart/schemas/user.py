de225041eed3d3267057f670166147b151e82206
liviamrocha <liviarochaa00@gmail.com>
2022-10-07 21:33:12 -0300

    refactor: Add extra settings to Swaegger examples in user schemas

--- shopping_cart/schemas/user.py
+++ shopping_cart/schemas/user.py
@@ -2,23 +2,57 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 from pydantic.networks import EmailStr
 
-# Modelo base de um usuário (para cadastro)
 class UserSchema(BaseModel):
-    name: str = Field(None, min_length=3, max_length=100)
-    email: EmailStr = Field(unique=True, index=True)
-    password: str
-    is_active: bool = Field(default=True)
-    is_admin: bool = Field(default=False)
-    created_at: datetime = datetime.now()
-    
-# Modelo de retorno de um documento de usuário
-def user_helper(user) -> dict:
-    return{
-        "id": str(user["_id"]),
-        "name": user["name"],
-        "email": user["email"],
-        "password": user["password"],
-        "is_active": user["is_active"],
-        "is_admin": user["is_admin"],
-        "created_at": user["created_at"],
-    }
+    name: str = Field(
+        min_length=3, 
+        max_length=100,
+        description="Nome do usuário"
+    )
+    email: EmailStr = Field(
+        unique=True, 
+        index=True,
+        description="E-mail do usuário"
+    )
+    password: str = Field(
+        description="Senha do usuário"
+    )
+    is_active: bool = Field(
+        default=True,
+        description="Usuário ativo?"
+    )
+    is_admin: bool = Field(
+        default=False,
+        description="Usuário administrador?"
+    )
+    created_at: datetime = Field(
+        default=datetime.now(),
+        description="Data/hora de criação do usuário"
+    )
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "string",
+                "email": "string",
+                "password": "string",
+                "is_active": True,
+                "is_admin": False
+            }
+        }
+
+class PasswordUpdateSchema(BaseModel):
+    email: EmailStr = Field(
+        unique=True, 
+        index=True,
+        description="E-mail do usuário"
+    )
+    new_password: str = Field(
+        description="Senha nova"
+    )
+    # old_password: str = Field(
+    #     description="Senha antiga"
+    # )
+
+class UserResponse(UserSchema):
+    pass
+
