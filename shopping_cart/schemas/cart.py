9e06f7f20efcffdb7e728ae0d5ea6ceaa01aea55
PampSPP <pampspp@gmail.com>
2022-10-08 07:55:52 -0300

    correção no cart schema

--- shopping_cart/schemas/cart.py
+++ shopping_cart/schemas/cart.py
@@ -1,13 +1,12 @@
 from math import prod
 from pydantic import BaseModel, Field
-from shopping_cart.schemas.product import ProductSchema
-
+from shopping_cart.schemas.order_item import OrderItemSchema
 from shopping_cart.schemas.user import UserSchema
 
 
 class CartSchema(BaseModel):
     user: UserSchema
-    products: ProductSchema = []
+    order_items: OrderItemSchema = []
     total_quantity: int = 0
     total_price: float = 0
     paid: bool = Field(default=False)
@@ -16,15 +15,16 @@ class CartSchema(BaseModel):
 def cart_helper(cart) -> dict:
     products_list = []
     
-    for item in cart["products"]:
+    for item in cart["order_items"]:
         product = {
-            "name": item["name"],
-            "description": item["description"],
-            "price": item["price"],
-            "material": item["material"],
-            "inmetro": item["inmetro"],
-            "code": item["code"],
-            "stock": item["stock"],
+            "name": item["product"]["name"],
+            "description": item["product"]["description"],
+            "price": item["product"]["price"],
+            "material": item["product"]["material"],
+            "inmetro": item["product"]["inmetro"],
+            "code": item["product"]["code"],
+            "stock": item["product"]["stock"],
+            "quantity": item["quantity"],
         }
         products_list.append(product)
         
@@ -36,7 +36,7 @@ def cart_helper(cart) -> dict:
         "is_active": cart.user["is_active"],
         "is_admin": cart.user["is_admin"],
         "created_at": cart.user["created_at"],
-        "products": products_list,
+        "order_items": products_list,
         "total_quantity": cart["total_quantity"],
         "total_price": cart["total_price"],
         "paid": cart["paid"],
