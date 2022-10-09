bacff98819713dd5b91b9a97a03b62839c3c961b
PampSPP <pampspp@gmail.com>
2022-10-09 06:55:37 -0300

    funcionalidade create_order

--- shopping_cart/cruds/order.py
+++ shopping_cart/cruds/order.py
@@ -12,15 +12,24 @@ async def create_order(email: EmailStr):
         find_cart = await db.cart_db.find_one({"user.email": email})
         if not find_cart:
             return {"message": "Usuário não possui um carrinho"}
+        
+        # Valida se o usuário tem um endereço de entrega cadastrado
         address = await db.address_db.find_one({"user.email": email, "address.is_delivery": True})
         if not address:
             return {"message": "Usuário não possui endereço de entrega cadastrado"}
-        # TODO
+        
+        
+        
+        
         agora_vai = db.cart_db.aggregate([
             {"$match": { "user.email": email}},
-            {"$merge": {"into": "order"}},
-            {"$addFields": {"address": dict(address)}}
+            {"$addFields": {"address": dict(address)}},
+            {"$merge": {"into": "order"}}
         ])
+        agora_vai_lista = await agora_vai.to_list(length=100)
+        print(agora_vai)
+        print(agora_vai_lista)
+            
         return {"message": "vixi maria"}
             
         # Retona a mensagem de erro
