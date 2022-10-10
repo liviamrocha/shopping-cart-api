2ae1aef46bc94fa446d2f0d6441aa43c91aec042
liviamrocha <liviarochaa00@gmail.com>
2022-10-10 12:05:56 -0300

    refactor: Add extra settings to Swaegger examples in address schemas

--- shopping_cart/schemas/address.py
+++ shopping_cart/schemas/address.py
@@ -1,31 +1,17 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel, Field
 from shopping_cart.schemas.user import UserSchema
 
 
-# Modelo base de um endereço (para cadastro)
-class Address(BaseModel):
-    street: str = Field(None, min_length=3, max_length=200)
-    number: int 
-    city: str = Field(None, min_length=3, max_length=200)
-    state: str = Field(None, min_length=3, max_length=200)
-    zip_code: int
-    is_delivery: bool
-    
-# Modelo base de criação de uma lista de endereços do usuário
 class AddressSchema(BaseModel):
-    user: UserSchema
-    address: List[Address]
+    city: str = Field(min_length=3, max_length=200)
+    state: str = Field(min_length=3, max_length=200)
+    zip_code: str
+    street: str = Field(min_length=3, max_length=200)
+    number: int 
+    complement: Optional[str] 
+    is_delivery: bool = Field(default=True)
 
-# Modelo de retorno de um documento de endereço com os dados do usuário
-def address_helper(address) -> dict:
-    user_address = {
-        "id": str(address["_id"]),
-        "name": address["user"]["name"],
-        "email": address["user"]["email"],
-        "address": [], 
-    }
-    for item in address["address"]:
-        user_address["address"].append(item)
-    return user_address
-    
+class AdressUpdateSchema(AddressSchema):
+    pass
+
