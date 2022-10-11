91502db187819dd632ca2eed195b3a990a3ac5e6
PampSPP <pampspp@gmail.com>
2022-10-11 16:55:23 -0300

    funcionalidade autorizar user - ainda a descobrir o erro

--- shopping_cart/auth/auth_handler.py
+++ shopping_cart/auth/auth_handler.py
@@ -1,12 +1,12 @@
 from decouple import config
 from pydantic import BaseSettings
-
+from decouple import config
 from shopping_cart.core import settings
 
 class Settings(BaseSettings):
-    JWT_SECRET_KEY = config("secret")
-    JWT_ALGORITHM = config("algorithm")
-    JWT_REFRESH_SECRET_KEY = config("refresh_secret")
+    JWT_SECRET_KEY: str = config("JWT_SECRET_KEY", cast=str)
+    JWT_ALGORITHM: str =  config("JWT_ALGORITHM", cast=str)
+    JWT_REFRESH_SECRET_KEY: str = config("JWT_REFRESH_SECRET_KEY", cast=str)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
     
