9543eb078377904f5886894677bf41286e1bd90c
liviamrocha <liviarochaa00@gmail.com>
2022-10-11 23:26:18 -0300

    feat: Replaces CRUD layer functions with validation layer functions for all cart routes

--- shopping_cart/routers/cart.py
+++ shopping_cart/routers/cart.py
@@ -1,30 +1,56 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from pydantic import EmailStr
-from shopping_cart.cruds.cart import add_product_cart, create_cart, find_cart_by_email, remove_product_cart
-
-from shopping_cart.schemas.cart import CartSchema
+from shopping_cart.controllers.cart import (
+    fetch_cart_by_email, 
+    add_cart_item,
+    remove_cart_item, 
+    clean_user_cart
+)
+from shopping_cart.schemas.cart import CartItemSchema, CartSchema, CartRequestSchema, CartResponseSchema
 from shopping_cart.schemas.order_item import OrderItemSchema
 from shopping_cart.schemas.user import UserSchema
 
 router = APIRouter(tags=['Carts'], prefix='/carts')
 
-@router.post('')
-async def add_cart(cart: CartSchema):
-    message = await create_cart(cart)
-    return message
+# @router.post('')
+# async def add_cart(cart: CartSchema):
+#     message = await create_cart(cart)
+#     return message
 
-@router.post('/item')
-async def add_item(email: EmailStr, code: int, quantity: int):
-    message = await add_product_cart(email, code, quantity)
-    return message
+@router.post(
+    '/', 
+    summary="Add item to a cart",
+    description="Add new item to user's shopping cart",
+    status_code=status.HTTP_201_CREATED,
+)
+async def add_item(email: EmailStr, cart_item: CartRequestSchema):
+    return await add_cart_item(email, cart_item)
 
-@router.put('/item')
-async def remove_item(email: EmailStr, code: int, quantity: int):
-    message = await remove_product_cart(email, code, quantity)
-    return message
-
-@router.get('')
+@router.get(
+    '/', 
+    response_model=CartResponseSchema,
+    summary="Get shopping cart",
+    description="Returns user's shopping cart",
+)
 async def get_cart_by_email(email: EmailStr):
-    message = await find_cart_by_email(email)
-    return message
+    cart = await fetch_cart_by_email (email)
+    return cart
+
+@router.delete(
+    "/",
+    status_code=status.HTTP_202_ACCEPTED,
+    summary="Delete item",
+    description="Remove item from user's shopping cart",
+)
+async def remove_item(email: EmailStr, cart_data: CartRequestSchema):
+    return await remove_cart_item(email, cart_data)
+
+@router.delete(
+    "/all",
+    status_code=status.HTTP_202_ACCEPTED,
+    summary="Clean cart",
+    description="Removes all items from the shopping cart",
+)
+async def clean_cart(email: EmailStr):
+    return await clean_user_cart(email)
